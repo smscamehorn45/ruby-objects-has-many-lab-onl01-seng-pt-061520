ace3c63f2de4c5e6d1587f3851ffbf6af164fd1b
Sean Scamehorn <smscamehorn45@gmail.com>
2020-07-13 23:45:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,23 +1,22 @@
-class Song 
-  attr_accessor :name, :artist 
+class Song
+  attr_accessor :name, :genre, :artist
 
   @@all = []
-
-  def initialize(name, artist=nil)
+  def initialize(name)
     @name = name
-    @artist = artist
     @@all << self
-  end
-
-  def artist_name
-    if self.artist
-      self.artist.name
-    else
-      nil
-    end
   end
 
   def self.all
     @@all
   end
+
+  def artist_name
+    if artist == nil
+      nil
+    else
+      self.artist.name
+    end
+  end
+
 end
