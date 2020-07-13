57fa9bdce9378623ae17898ef756123fa39c926a
Sean Scamehorn <smscamehorn45@gmail.com>
2020-07-13 23:45:32 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,16 +1,27 @@
 class Post
-  attr_accessor :author, :title 
+  attr_accessor :title, :author
 
-  def initialize(title)
+  @@all = []
+
+  def initialize (title)
     @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def author_name
-    if author 
-      self.author.name 
-    else
+    if author == nil
       nil
+    else
+      self.author.name
     end
   end
 
-end 
+  def add_post(post)
+    post.author = self
+  end
+
+end
